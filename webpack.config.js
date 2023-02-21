30fc9f6e44020e252962373e9ff4a117f122c4b0
olhavic <saul.yerbt@gmail.com>
2023-02-21 21:53:42 +0200

    initinal webpack

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, "./src/js/modal.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "[name].bundle.js",
+  },
+  mode: "development",
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "./dist"),
+    },
+    historyApiFallback: true,
+    compress: true,
+    port: 4040,
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "webpack Boilerplate",
+      template: path.resolve(__dirname, "./src/index.html"), // template file
+      filename: "index.html", // output file
+    }),
+    // new webpack.HotModuleReplacementPlugin(),
+    // new CleanWebpackPlugin(),
+  ],
+};
