4a4fc4accf37a25f165c462b5f11249aaa88b358
OlhaPylyp <olhasvet@gmail.com>
2023-02-22 15:13:10 +0200

    added test intoplugin

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,54 @@ module.exports = {
         test: /\.scss$/,
         use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
       },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              name: "[path][name].[ext]",
+              limit: 8192,
+              esModule: false,
+            },
+          },
+          "img-loader",
+        ],
+      },
+      {
+        test: /\.woff(2)?(\?[a-z0-9#=&.]+)?$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "fonts/",
+              limit: 10000,
+              mimetype: "application/font-woff",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "fonts/",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: "html-loader",
+      },
+      {
+        test: /\.hbs$/,
+        use: "handlebars-loader",
+      },
     ],
   },
   plugins: [
